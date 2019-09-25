b301b18776d8a4fc39b9afec260d3721f7313938
Grig Alex <alexengrigdev@gmail.com>
2019-09-25 23:45:53 +0300

    Add bad implementation

--- src/main/java/io/github/alexengrig/jtor/intellij/plugin/DemoAction.java
+++ src/main/java/io/github/alexengrig/jtor/intellij/plugin/DemoAction.java
@@ -9,10 +9,11 @@ import com.intellij.openapi.editor.CaretModel;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.TextRange;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class DemoAction extends AnAction {
     @Override
@@ -22,14 +23,64 @@ public class DemoAction extends AnAction {
         final Document document = editor.getDocument();
         CaretModel caretModel = editor.getCaretModel();
         List<Caret> carets = caretModel.getAllCarets();
-        for (Caret caret : carets) {
-            int start = caret.getSelectionStart();
-            int end = caret.getSelectionEnd();
-            String text = document.getText(TextRange.create(start, end));
-            WriteCommandAction.runWriteCommandAction(project, () ->
-                    document.insertString(start, text.toUpperCase())
-            );
-            caret.removeSelection();
+        List<Integer> lines = carets.stream()
+                .map(Caret::getLogicalPosition)
+                .map(logicalPosition -> logicalPosition.line)
+                .collect(Collectors.toList());
+        List<Integer> offsets = carets.stream()
+                .map(Caret::getOffset)
+                .collect(Collectors.toList());
+        List<String> texts = carets.stream()
+                .map(Caret::getSelectedText)
+                .collect(Collectors.toList());
+        int count = lines.size();
+        int step = getAverageStep(lines);
+        List<String> generated = getGenerated(texts);
+        Integer line = lines.get(lines.size() - 1);
+        for (int i = 0; i < count; i++) {
+            Integer offset = offsets.get(i);
+            String text = generated.get(i);
+            WriteCommandAction.runWriteCommandAction(project, () -> {
+                document.insertString(offset, text);
+            });
         }
+
+        carets.forEach(Caret::removeSelection);
+    }
+
+    private int getAverageStep(List<Integer> lines) {
+        List<Integer> steps = new ArrayList<>();
+        for (int i = 1, length = lines.size(); i < length; i++) {
+            steps.add(lines.get(i) - lines.get(i - 1));
+        }
+        return (int) steps.stream()
+                .mapToInt(Integer::intValue)
+                .average()
+                .orElse(0);
+    }
+
+    private List<String> getGenerated(List<String> texts) {
+        List<Integer> numbers = new ArrayList<>();
+        for (String text : texts) {
+            String[] split = text.split("\\D+");
+            numbers.add(Integer.valueOf(split[1]));
+        }
+        List<Integer> steps = new ArrayList<>();
+        for (int i = 1, length = numbers.size(); i < length; i++) {
+            steps.add(numbers.get(i) - numbers.get(i - 1));
+        }
+        int step = (int) steps.stream()
+                .mapToInt(Integer::intValue)
+                .average()
+                .orElse(0);
+        Integer current = numbers.get(numbers.size() - 1);
+        List<String> results = new ArrayList<>();
+        for (String text : texts) {
+            String[] split = text.split("\\d+");
+            current += step;
+            String result = split[0] + current;
+            results.add(result);
+        }
+        return results;
     }
 }
